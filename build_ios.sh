1d7656f27751f9b45a31c4bfcc6f20f51964736a
vvanghelue <voiture32>
2015-02-26 14:27:21 +0100

    first commit

--- build_ios.sh
+++ build_ios.sh
@@ -1,10 +1,10 @@
 rm -rf www/*
-cp -R www_grunt/app www/app
-cp -R www_grunt/assets www/assets
-cp -R www_grunt/build www/build
-cp -R www_grunt/lib www/lib
-cp -R www_grunt/tpl www/tpl
-cp -R www_grunt/app.js www/app.js
-cp -R www_grunt/index.html www/index.html
+cp -R src/app www/app
+cp -R src/assets www/assets
+cp -R src/build www/build
+cp -R src/lib www/lib
+cp -R src/tpl www/tpl
+cp -R src/app.js www/app.js
+cp -R src/index.html www/index.html
 
 cordova build ios
--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+rm -rf www/*
+cp -R src/app www/app
+cp -R src/assets www/assets
+cp -R src/build www/build
+cp -R src/lib www/lib
+cp -R src/tpl www/tpl
+cp -R src/app.js www/app.js
+cp -R src/index.html www/index.html
 
 cordova platform remove ios
 cordova platform remove android
